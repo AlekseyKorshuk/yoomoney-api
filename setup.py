5599e0c8cf166235c899f0fccf44d45c33c43485
AlekseyKorshuk <48794610+KorshukAleksey@users.noreply.github.com>
2021-05-21 14:53:09 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='YooMoney',
+    version='1.0.0',
+    packages=['yoomoney', 'yoomoney.account', 'yoomoney.history', 'yoomoney.quickpay', 'yoomoney.authorize',
+              'yoomoney.operation', 'yoomoney.operation_details'],
+    url='https://github.com/AlekseyKorshuk/yoomoney-api',
+    license='GPL-3.0',
+    author='AlekseyKorshuk',
+    author_email='',
+    description='Unofficial YooMoney API python library'
+)
