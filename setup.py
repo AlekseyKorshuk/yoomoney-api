b8a58881166af9ed58fa5d3cfa785ca8a7d98596
Aleksey Korshuk <48794610+AlekseyKorshuk@users.noreply.github.com>
2025-01-29 14:59:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name='YooMoney',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=['tests']),
     url='https://github.com/AlekseyKorshuk/yoomoney-api',
     license='GPL-3.0',
